ae4b1acf235ee40ca75fd77672eecb8a765dfc74
JoaoVictor-Guerra <84593164+JoaoVictor-Guerra@users.noreply.github.com>
2022-09-25 21:39:27 -0300

    Backend (Spring) added

---
+++ lab02/backend/src/test/java/com/example/aluguelDeCarros/AluguelDeCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aluguelDeCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelDeCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
