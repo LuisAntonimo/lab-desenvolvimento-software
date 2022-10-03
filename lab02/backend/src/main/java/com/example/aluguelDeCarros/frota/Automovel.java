a97122edc55be52c8d26fb28e1e1c5ce873c3c46
Luís Antônio <luisantoniossouza@gmail.com>
2022-10-02 23:54:02 -0300

    setup básico

--- lab02/backend/src/main/java/com/example/aluguelDeCarros/frota/Automovel.java
+++ lab02/backend/src/main/java/com/example/aluguelDeCarros/frota/Automovel.java
@@ -1,5 +1,73 @@
 package com.example.aluguelDeCarros.frota;
 
 public class Automovel {
+
+    private int matricula, ano, notaCondicao;
+    private String marca, modelo, placa;
+
+    public Automovel(int matricula, int ano, int notaCondicao, String marca, String modelo, String placa) {
+      this.matricula = matricula;
+      this.ano = ano;
+      this.notaCondicao = notaCondicao;
+      this.marca = marca;
+      this.modelo = modelo;
+      this.placa = placa;
+    }
+
+    // getters
+
+    public int getMatricula() {
+      return matricula;
+    }
+   
+    public int getAno() {
+      return ano;
+    }
+
+    public int getNotaCondicao() {
+      return notaCondicao;
+    }
     
+    public String getMarca() {
+      return marca;
+    }
+    
+    public String getModelo() {
+      return modelo;
+    }
+    
+    public String getPlaca() {
+      return placa;
+    }
+
+    // setters
+
+    public void setMatricula(int matricula) {
+      this.matricula = matricula;
+    }
+
+    public void setAno(int ano) {
+      this.ano = ano;
+    }
+
+    public void setNotaCondicao(int notaCondicao) {
+      this.notaCondicao = notaCondicao;
+    }
+
+    public void setMarca(String marca) {
+      this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+      this.modelo = modelo;
+    }
+
+    public void setPlaca(String placa) {
+      this.placa = placa;
+    }
+
+
+    public void avaliarEstadoAutomovel(int notaCondicao) {
+      this.setNotaCondicao(notaCondicao);
+    }
 }
--- lab02/backend/src/main/java/com/example/aluguelDeCarros/utilizador/Usuario.java
+++ lab02/backend/src/main/java/com/example/aluguelDeCarros/utilizador/Usuario.java
@@ -1,5 +1,38 @@
 package com.example.aluguelDeCarros.utilizador;
 
 public abstract class Usuario {
+    private String email, senha;
+
+    public String getEmail() {
+      return email;
+    }
+
+    public String getSenha() {
+      return senha;
+    }
+
+
+    public void setEmail(String email) {
+      this.email = email;
+    }
+
+    public void setSenha(String senha) {
+      this.senha = senha;
+    }
+
     
+    public void logar(String email, String senha) {
+
+    }
+
+    public void cadastrar(String email, String senha) {
+      this.setEmail(email);
+      this.setSenha(senha);
+    }
+
+    public void modificarPedido() {};
+
+    private boolean isValido() {
+      return true;
+    }
 }
